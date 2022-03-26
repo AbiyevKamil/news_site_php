54e74f2c43e8e359b7f16cdcf1354f2e55a0d2df
Kamil Abiyev <kamilabiyev1903@gmail.com>
2022-03-26 18:09:53 +0400

    seed data implemented

---
+++ queries/seed/fetch.php
@@ -0,0 +1,33 @@
+<?php
+include "../../libs/simple_html_dom.php";
+
+function fetchData()
+{
+  $data = array();
+  $base_url = "https://report.az/";
+  $html = file_get_html($base_url);
+  foreach ($html->find("div.rest-news<div.news-block") as $news_block) {
+    $title = $news_block->find("div.info>a")[0]->plaintext;
+    // $date = $news_block->find("div.news-date>span")[0]->plaintext . " " . $news_block->find("div.news-date>span")[1]->plaintext;
+    $img = $news_block->find("div.image>a>img")[0]->attr['data-src'];
+    $details_link = "https://report.az" . $news_block->find("div.image>a")[0]->attr['href'];
+    $details_html = file_get_html($details_link);
+    $details = "";
+    foreach ($details_html->find("div.editor-body<p") as $element) {
+      $details = $details . $element->plaintext;
+    }
+    $details;
+    $single = array(
+      "title" => $title,
+      // "date" => $date,
+      "img" => $img,
+      "details" => $details,
+      // "author" => $base_url,
+    );
+    if (count($data) == 15)
+      break;
+    array_push($data, $single);
+  }
+  return $data;
+}
+fetchData();
---
+++ queries/seed/seed.php
@@ -0,0 +1,34 @@
+<?php
+
+function seedData()
+{
+  include "../../config/db.php";
+  include "./fetch.php";
+
+  if ($connection) {
+    $sqlForNewsCount = "select count(*) as count from news";
+    $sqlForUserCount = "select count(*) as count,id from users where `email`='report@gmail.com'";
+    $sqlToAddUser = "INSERT INTO users (`email`, `full_name`, `user_name`, `password`, `is_approved`) VALUES ('report@gmail.com', 'Report Az', 'report.az', '123456', b'1');";
+
+    $fetchedUsers = mysqli_query($connection, $sqlForUserCount);
+    $user = mysqli_fetch_assoc($fetchedUsers);
+    if ($user["count"] == 0) {
+      mysqli_query($connection, $sqlToAddUser);
+    }
+    $fetchedNews = mysqli_query($connection, $sqlForNewsCount);
+    $countNews = mysqli_fetch_assoc($fetchedNews)["count"];
+    if ($countNews == 0) {
+      $user_id = $user["id"];
+      $data = fetchData();
+      if (count($data) > 0) {
+        foreach ($data as $single) {
+          $title = $single['title'];
+          $img = $single['img'];
+          $details = $single['details'];
+          $sqlToAddNews = "INSERT INTO `news` (`title`, `content`, `banner`, `user_id`) VALUES ('$title', '$details', '$img', '$user_id');";
+          mysqli_query($connection, $sqlToAddNews);
+        }
+      }
+    }
+  }
+}
