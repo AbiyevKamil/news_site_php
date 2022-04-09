30ac0610ba9394e228c680947be21999478fa737
Habil99 <abiyevhabil3@gmail.com>
2022-04-09 20:05:36 +0400

    database configuration changed to kamil's settings

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 $DB = array(
   "hostname" => "localhost",
   "username" => "root",
-  "password" => "nocode0911",
-  "database" => "news",
+  "password" => "akamil2002",
+  "database" => "news_site",
 );
 
 $connection = mysqli_connect($DB["hostname"], $DB["username"], $DB["password"], $DB["database"]);
