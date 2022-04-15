fdab7411b78ffef44707d21f0fb54c2b63c0a44f
habil99 <abiyevhabil3@gmail.com>
2022-04-15 08:54:57 +0400

    profile.php div.row added

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 $DB = array(
   "hostname" => "localhost",
   "username" => "root",
-  "password" => "nocode0911",
-  "database" => "news_app",
+  "password" => "kamil2002",
+  "database" => "news",
 );
 
 $connection = mysqli_connect($DB["hostname"], $DB["username"], $DB["password"], $DB["database"]);
--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 
-    include "./components/header.php"; 
-    include "/AppServ/www/sdf/news_site_php/auth/getProfileData.php";
+include "./components/header.php";
+include "/AppServ/www/sdf/news_site_php/auth/getProfileData.php";
 
 ?>
 <div class="pt-55 section">
@@ -17,61 +17,63 @@
       </p>
     </div>
     <h3 style="margin-bottom: 60px;">News</h3>
-    <?php
-    if (count(getPosts($connection, $user_id)) == 0) {
-    ?>
-      <div class="alert alert-info p-4">
-        There is no posted news from <?= getUsername($pro) ?>.
-      </div>
+    <div class="row">
       <?php
-    } else {
-      foreach (getPosts($connection, $user_id) as $item) {
+      if (count(getPosts($connection, $user_id)) == 0) {
       ?>
-        <div class="col-lg-4 col-md-6">
-          <!--Post-1-->
-          <div class="post-card">
-            <div class="post-card-image">
-              <a href="news.php?newsId=<?= $item["id"] ?>">
-                <img src="public/uploads/news/<?= $item["banner"] ?>" alt="">
-              </a>
-            </div>
-            <div class="post-card-content">
-              <a class="categorie">
-                <?= $item["category_name"] ?>
-              </a>
-              <h5>
-                <a href="news.php?newsId=<?= $item["id"] ?>"><?= $item["title"] ?></a>
-              </h5>
-              <p>
-                <?php
-                if (strlen($item["content"]) > 50)
-                  echo substr($item["content"], 0, 50) . "...";
-                else
-                  echo $item["content"];
-                ?>
-              </p>
-              <div class="post-card-info">
-                <ul class="list-inline">
-                  <li>
-                    <a href="user.php?userId=<?= getID($pro) ?>">
-                      <img src="public/uploads/users/<?= getPicture($pro) ?>" alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#"><?= getUsername($pro) ?></a>
-                  </li>
-                  <li class="dot"></li>
-                  <li><?= $item["created_at"] ?></li>
-                </ul>
+        <div class="alert alert-info p-4">
+          There is no posted news from <?= getUsername($pro) ?>.
+        </div>
+        <?php
+      } else {
+        foreach (getPosts($connection, $user_id) as $item) {
+        ?>
+          <div class="col-lg-4 col-md-6">
+            <!--Post-1-->
+            <div class="post-card">
+              <div class="post-card-image">
+                <a href="news.php?newsId=<?= $item["id"] ?>">
+                  <img src="public/uploads/news/<?= $item["banner"] ?>" alt="">
+                </a>
+              </div>
+              <div class="post-card-content">
+                <a class="categorie">
+                  <?= $item["category_name"] ?>
+                </a>
+                <h5>
+                  <a href="news.php?newsId=<?= $item["id"] ?>"><?= $item["title"] ?></a>
+                </h5>
+                <p>
+                  <?php
+                  if (strlen($item["content"]) > 50)
+                    echo substr($item["content"], 0, 50) . "...";
+                  else
+                    echo $item["content"];
+                  ?>
+                </p>
+                <div class="post-card-info">
+                  <ul class="list-inline">
+                    <li>
+                      <a href="user.php?userId=<?= getID($pro) ?>">
+                        <img src="public/uploads/users/<?= getPicture($pro) ?>" alt="" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#"><?= getUsername($pro) ?></a>
+                    </li>
+                    <li class="dot"></li>
+                    <li><?= $item["created_at"] ?></li>
+                  </ul>
+                </div>
               </div>
             </div>
+            <!--/-->
           </div>
-          <!--/-->
-        </div>
-    <?php
+      <?php
+        }
       }
-    }
-    ?>
+      ?>
+    </div>
   </div>
 </div>
 
